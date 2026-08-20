13b6e4077f09501f6926bc8629679453ebc550be
Mansi Rathore <mansirathore7668@gmail.com>
2026-08-20 18:01:21 +0530

    Time: 7 ms (100%), Space: 46.1 MB (26.95%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -6,11 +6,6 @@ class Solution {
         while(n%3 == 0){
             n=n/3;
         }
-        if(n==1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (n==1);
     }
 }
